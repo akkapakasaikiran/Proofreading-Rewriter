e3c0f38e0ff374b99307b9d385055fa548aaca6b
akkapakasaikiran <180050005@iitb.ac.in>
2019-10-30 13:43:44 +0530

    demarau distance added

--- spellcheck/spellcheck.py
+++ spellcheck/spellcheck.py
@@ -33,7 +33,7 @@ def valid_word(word_inp):
 	return d_uk.check(word_inp) or d_us.check(word_inp) or d_us.check(cap_word) or d_uk.check(cap_word)
 
 def P(variate):
-	return edit_distance(word, variate);
+	return damerau_levenshtein_distance(word, variate);
 
 def correction():
 	"Most probable spelling corrections for word."
@@ -85,6 +85,32 @@ def edit_distance(s1,s2):
 			else:
 				dp[i, j] =  min(dp[i, j - 1]+1, dp[i - 1, j]+1, dp[i - 1, j - 1]+subs_factor*euclidean_distance(s1[i-1],s2[j-1]))
 	return dp[M, N]
+
+def damerau_levenshtein_distance(s1, s2):
+    d = {}
+    lenstr1 = len(s1)
+    lenstr2 = len(s2)
+    for i in range(-1,lenstr1+1):
+        d[(i,-1)] = i+1
+    for j in  range(-1,lenstr2+1):
+        d[(-1,j)] = j+1
+
+    for i in range(lenstr1):
+        for j in range(lenstr2):
+            if s1[i] == s2[j]:
+                cost = 0
+            else:
+                cost = euclidean_distance(s1[i],s2[j])
+            d[(i,j)] = min(
+                           d[(i-1,j)] + 1, # deletion
+                           d[(i,j-1)] + 1, # insertion
+                           d[(i-1,j-1)] + cost, # substitution
+                          )
+            if i and j and s1[i]==s2[j-1] and s1[i-1] == s2[j]:
+                d[(i,j)] = min (d[(i,j)], d[i-2,j-2] + cost) # transposition
+
+    return d[lenstr1-1,lenstr2-1]
+
  
 # reading input file
 fullfile = open(sys.argv[1], "r").read()
